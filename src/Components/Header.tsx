34367c13b9ff22bea924b487944ac7ef3f904d4b
Nikita Kuznetsov <nikitacs2912@mail.ru>
2026-04-01 00:25:36 +0300

    feet: Upgrade mobile menu and adaptive

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -9,28 +9,40 @@ const mobileStyleButton = "font-[500] "
 
 export default function Header(){
     const [isMenuOpen, setMenuOpen]= useState<boolean>(false)
+
+    const changeStateMenu = () =>{
+        isMenuOpen ? setMenuOpen(false) : setMenuOpen(true)
+    }
     return(
         <header className="w-full min-h-[985px] max-h-[985px] bg-cover"style={{
             backgroundImage: `url(${headerBg})`
         }}>
             <div className="
+            pt-[20px]
+            pl-[16px]
+            pr-[16px]
             text-[#FDFDFD]
             flex 
+            text-center
             justify-between
             w-[100%] 
             font-[500]
-            align-center 
+            align-center
             md:justify-center 
+            md:max-w-[700px]
+            md:text-[12px]
+            md:font-[700]
             mx-[auto] 
-            lg:max-w-[900px]
+            lg:max-w-[1000px]
+            lg:text-[18px]
+            lg:font-[500]
             lg:gap-[20px]
-            xl:max-w-[1000px]
-            2xl:max-w-[1180px]
+            2xl:max-w-[1280px]
             2xl:flex
             2xl:text-[18px]
             2xl:gap-[212px]
             ">
-                <img className="m-[10px] md:p-[0px] w-[84px] h-[44px] md:w-[137px] md:h-[72px]" src={logo} alt="" />
+                <img className=" md:p-[0px] w-[84px] h-[44px] md:w-[137px] md:h-[72px]" src={logo} alt="" />
                 <div className="md:flex gap-[24px] my-[auto] hidden ">
                     <button className="hoverHeader">Главная</button>
                     <button className="hoverHeader">Про гида</button>
@@ -41,19 +53,21 @@ export default function Header(){
                     <button className="bg-[#1A3E3E] hover:bg-[#FDFDFD] transition duration-300 hover:border hover:border-#1A3E3E hover:text-[#1A3E3E] rounded-[4px] w-[157px] h-[44px]" >Консультация</button>
                 </div>
 
-                <div className="grid md:hidden ">
-                    <div className="">
-                        <img className="ml-auto p-[10px]" src={isMenuOpen ? closeMenu : iconMenu} alt="" />
+                <div className="grid md:hidden">
+                    <div className="my-auto">
+                        <img className="ml-auto w-[25px]" onClick={changeStateMenu} src={isMenuOpen ? closeMenu : iconMenu} alt="" />
                     </div>
-                    <div className="grid grid-cols-1 pr-[10px]">
-                        <button className={mobileStyleButton}>Главная</button>
-                        <button className={mobileStyleButton}>Про гида</button>
-                        <button className={mobileStyleButton}>Программа тура</button>
-                        <button className={mobileStyleButton} >Стоимость</button>
-                        <button className={mobileStyleButton}>Блог</button>
-                        <button className={mobileStyleButton}>Контакты</button>   
-                        <button className={mobileStyleButton} >Консультация</button>
-                    </div>
+                    {isMenuOpen && (
+                        <div className="grid grid-cols-1 pr-[10px] w-full  bg-[#1A3E3E]">
+                            <button className={mobileStyleButton}>Главная</button>
+                            <button className={mobileStyleButton}>Про гида</button>
+                            <button className={mobileStyleButton}>Программа тура</button>
+                            <button className={mobileStyleButton} >Стоимость</button>
+                            <button className={mobileStyleButton}>Блог</button>
+                            <button className={mobileStyleButton}>Контакты</button>   
+                            <button className={mobileStyleButton} >Консультация</button>
+                        </div>
+                    )}
                 </div>
             </div>
         </header>
